bdfa6fafcc51f3b60a2933995d74677d96129543
adrousth <adrousth@sbcglobal.net>
2022-06-22 10:44:41 -0500

    adding basic controller methods based on requirements

--- testing_stuff.py
+++ testing_stuff.py
@@ -1,1 +1,34 @@
-print("hello"[2:4])
+from flask import Flask, request
+
+from faker import Faker
+
+fake = Faker()
+
+app = Flask(__name__)
+users = {
+    10001: {
+        "first name": "joe",
+        "last name": "smith"
+    },
+    10002: {
+        "first name": "steve",
+        "last name": "johnson"
+    }
+
+}
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return users
+
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    print("add user")
+    data = request.get_json()
+    print(data)
+    return {}
+
+
+app.run(port=8081)
